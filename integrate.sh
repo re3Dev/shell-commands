9548ee9d2770b192076b247053dd32dca2692fb7
Mitchell-Mashburn <mitchell@re3d.org>
2024-03-05 13:07:36 -0600

    Refactor install script - cleanup

--- integrate.sh
+++ integrate.sh
@@ -1,37 +1,42 @@
 #!/bash/bin
+
+# Define variables
 home=/home/pi
-sys_path=/home/pi/printer_data/config/src/system_config
-config=/home/pi/printer_data/config
+config=$home/printer_data/config
+sys_path=$config/src/system_config
+config_files=("standalone.cfg" "crowsnest.conf" "timelapse.cfg" "src/paths.py" "shell_command.cfg" ".master.cfg")
+
+# Update and upgrade system
 sudo apt update
 sudo apt upgrade -y
-cd /home/pi/klipper_config/
+
+# Pull latest changes from git
+cd $home/klipper_config/
 git pull
 cd $home
-sleep 1
+
+# Backup config
 rm -rf $home/configBKP
 mkdir $home/configBKP
 chown pi:pi $home/configBKP
-cp -a $home/printer_data/config/. $home/configBKP/
-cd /home/pi
-mv $home/printer_data/config/standalone.cfg $home/standaloneTMP.cfg
-mv $home/printer_data/config/crowsnest.conf $home/crowsnestTMP.conf
-mv $home/printer_data/config/timelapse.cfg $home/timelapseTMP.cfg
-mv $home/printer_data/config/src/paths.py $home/pathsTMP.py
-mv $home/printer_data/config/shell_command.cfg $home/shell_commandTMP.cfg
-mv $home/printer_data/config/.master.cfg $home/.masterTMP.cfg
-rm -rf $home/printer_data/config
-cp -a $home/klipper_config/. $home/printer_data/config/
-mv $home/standaloneTMP.cfg $home/printer_data/config/standalone.cfg
-mv $home/crowsnestTMP.conf $home/printer_data/config/crowsnest.conf
-mv $home/timelapseTMP.cfg $home/printer_data/config/timelapse.cfg
-mv $home/pathsTMP.py $home/printer_data/config/src/paths.py
-mv $home/shell_commandTMP.cfg $home/printer_data/config/shell_command.cfg
-mv $home/.masterTMP.cfg $home/printer_data/config/.master.cfg
-chown pi:pi $config/standalone.cfg
-chown pi:pi $config/crowsnest.conf
-chown pi:pi $config/timelapse.cfg
-chown pi:pi $config/shell_command.cfg
-chown pi:pi $config/.master.cfg
+cp -a $config/. $home/configBKP/
+
+# Move config files to temporary location
+for file in "${config_files[@]}"; do
+    mv $config/$file $home/${file}TMP
+done
+
+# Replace config with latest from git
+rm -rf $config
+cp -a $home/klipper_config/. $config/
+
+# Move config files back from temporary location
+for file in "${config_files[@]}"; do
+    mv $home/${file}TMP $config/$file
+    chown pi:pi $config/$file
+done
+
+# Refresh display
 sleep 1
 export DISPLAY=:0
 export XAUTHORITY=/home/pi/.Xauthority
