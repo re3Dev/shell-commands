73f3e95b69fcc4a6babd83a07e073eb86ed871e7
Mitchell-Mashburn <mitchell@re3d.org>
2024-03-11 13:58:31 -0500

    Simplified code, overwrite whole file instead of redundant checks

--- change_configuration.sh
+++ change_configuration.sh
@@ -3,110 +3,65 @@
 # The path to your .master.cfg file
 config_file="/home/pi/printer_data/config/.master.cfg"
 
-# Function to update the section name
-update_section() {
-    local old_section=$1
-    local new_section=$2
-    # Using sed to replace the old section name with the new one
-    sed -i "s/\[$old_section\]/[$new_section]/" $config_file
-}
-
-# Function to update configuration parameters
+# Function to update the section and configuration parameters
 update_config() {
-    local param=$1
-    local value=$2
-    # Using sed to update the configuration file within the current section
-    sed -i "/^\[$current_section\]/,/^\[/ {s/^$param=.*$/$param=$value/}" $config_file
+    local section=$1
+    local platform_type=$2
+    local board_type=$3
+    local crammer_enabled=$4
+    local heater_bed_enabled=$5
+    
+    # Replace the entire file content with the new section and parameters
+    cat > $config_file <<EOF
+[$section]
+platform_type=$platform_type
+board_type=$board_type
+crammer_enabled=$crammer_enabled
+heater_bed_enabled=$heater_bed_enabled
+EOF
 }
 
 # Function to set parameters based on the input number
 set_combination() {
     local choice=$1
+    
     case $choice in
         1)
-            new_section="fff"
-            platform_type="regular"
-            board_type="archimajor"
-            crammer_enabled="false"
-            heater_bed_enabled="true"
+            update_config "fff" "regular" "archimajor" "false" "true"
             ;;
         2)
-            new_section="fff"
-            platform_type="xlt"
-            board_type="archimajor"
-            crammer_enabled="false"
-            heater_bed_enabled="true"
+            update_config "fff" "xlt" "archimajor" "false" "true"
             ;;
         3)
-            new_section="fff"
-            platform_type="terabot"
-            board_type="archimajor"
-            crammer_enabled="false"
-            heater_bed_enabled="true"
+            update_config "fff" "terabot" "archimajor" "false" "true"
             ;;
         4)
-            new_section="fgf"
-            platform_type="regular"
-            board_type="archimajor"
-            crammer_enabled="false"
-            heater_bed_enabled="false"
+            update_config "fgf" "regular" "archimajor" "false" "false"
             ;;
         5)
-            new_section="fgf"
-            platform_type="xlt"
-            board_type="archimajor"
-            crammer_enabled="false"
-            heater_bed_enabled="false"
+            update_config "fgf" "xlt" "archimajor" "false" "false"
             ;;
         6)
-            new_section="fgf"
-            platform_type="terabot"
-            board_type="archimajor"
-            crammer_enabled="false"
-            heater_bed_enabled="false"
+            update_config "fgf" "terabot" "archimajor" "false" "false"
             ;;
         7)
-            new_section="fgf"
-            platform_type="regular"
-            board_type="archimajor"
-            crammer_enabled="true"
-            heater_bed_enabled="false"
+            update_config "fgf" "regular" "archimajor" "true" "false"
             ;;
         8)
-            new_section="fgf"
-            platform_type="xlt"
-            board_type="archimajor"
-            crammer_enabled="true"
-            heater_bed_enabled="false"
+            update_config "fgf" "xlt" "archimajor" "true" "false"
             ;;
         9)
-            new_section="fgf"
-            platform_type="terabot"
-            board_type="archimajor"
-            crammer_enabled="true"
-            heater_bed_enabled="false"
+            update_config "fgf" "terabot" "archimajor" "true" "false"
             ;;
+        # Add more combinations as needed
         *)
             echo "Invalid combination number. Please enter a number between 1-9."
             exit 1
             ;;
     esac
 
-    # Update the section name
-    update_section $current_section $new_section
-    current_section=$new_section
-
-    # Update the config file with the selected combination
-    update_config "platform_type" "$platform_type"
-    update_config "board_type" "$board_type"
-    update_config "crammer_enabled" "$crammer_enabled"
-    update_config "heater_bed_enabled" "$heater_bed_enabled"
-
-    echo "Configuration updated to combination $choice with section [$new_section] successfully."
+    echo "Configuration updated to combination $choice successfully."
 }
-
-# Initial section name (you can change it as needed based on the default or expected section name before running the script)
-current_section="fff"
 
 # Check if an argument is provided
 if [[ $# -ne 1 ]]; then
