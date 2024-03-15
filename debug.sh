5a4d568cc26a9331a98eaf191a4f846589ee4135
Mitchell-Mashburn <mitchell@re3d.org>
2024-03-15 15:00:45 -0500

    Updated path for klipper log location

--- debug.sh
+++ debug.sh
@@ -3,10 +3,10 @@
 cd /home/pi/
 mkdir debugging
 cd /home/pi/debugging/
-cp /home/pi/klipper_logs/klippy.log .
+cp /home/pi/printer_data/logs/klippy.log .
 /home/pi/klipper/scripts/logextract.py  ./klippy.log
 cd /home/pi/
 sudo rm -rf /home/pi/printer_data/config/debugging
 sudo mv /home/pi/debugging /home/pi/printer_data/config/debugging
-/home/pi/klipper/scripts/graphstats.py /home/pi/klipper_logs/klippy.log -o /home/pi/printer_data/config/debugging/loadgraph.png
+/home/pi/klipper/scripts/graphstats.py /home/pi/printer_data/logs/klippy.log -o /home/pi/printer_data/config/debugging/loadgraph.png
 $echo "Debug information has been generated in the 'Debugging' folder in the configuration files"
